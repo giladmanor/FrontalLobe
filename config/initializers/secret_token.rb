40d5ed67b8006ef6455d0f44d2dde9a7987d1e71
Gilad Manor <gilad@quizic.co>
2012-10-07 12:29:07 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FrontalLobe::Application.config.secret_token = '174e7dd5e4af412bbff2c9526546472a38b83ec066241e9fe645b138f97272d856681bf02f79ad958703676b71f033a44d68a70a002bd2379e004562b36b0a07'
